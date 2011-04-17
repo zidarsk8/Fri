cede933e9f2300a1365836242a3c31bbd461b54e
Smotko <ensmotko@gmail.com>
2011-04-17 20:02:44 +0200

    Parsing vertices and normals

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import json
 
 if len(sys.argv) != 3:
     print "use like this: ./convert.py input_file output_file"
@@ -8,24 +9,30 @@ if len(sys.argv) != 3:
     
 f = open(sys.argv[1], 'r')
 
+def parse_xyz(str):
+    arr = l[2:].strip().split(' ')
+    return {'x':float(arr[0]), 'y':float(arr[1]), 'z':float(arr[2])}
 
+obj = {'name': "", 'vertices': [], 'normals': [], 'faces':{'type':0, 'vertices': [], 'normals':[]},}
 output = ""
 for l in f:
     if l[0] == 'o':
-        output += l[2:]
+        obj["name"] = l[2:].strip()
         
     if l[0] == 'v':
-        output += ""
-        
-    if l[0] == 'n':
-        output += ""   
+        obj['vertices'].append(parse_xyz(l[2:]))
         
     if l[0:2] == 'vn':
-        output += l[0:1]
+        obj['normals'].append(parse_xyz(l[3:]))
+        
+    if l[0:2] == 'f':
+        arr = l[2:].strip().split(' ')
+        if len(arr) == 1:
+            obj["faces"]
         
     if l[0] == 'n':
         output += ""     
         
-print output
+print json.dump(obj, open(sys.argv[2], 'w'))
     
 

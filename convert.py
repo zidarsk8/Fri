77ac331aee5ac1b52c95136d27be7fc1c66f2238
Smotko <ensmotko@gmail.com>
2011-04-17 20:24:03 +0200

    First version of convert.py *should* be working.

--- convert.py
+++ convert.py
@@ -13,7 +13,7 @@ def parse_xyz(str):
     arr = l[2:].strip().split(' ')
     return {'x':float(arr[0]), 'y':float(arr[1]), 'z':float(arr[2])}
 
-obj = {'name': "", 'vertices': [], 'normals': [], 'faces':{'type':0, 'vertices': [], 'normals':[]},}
+obj = {'name': "", 'vertices': [], 'normals': [], 'faces':[],}
 output = ""
 for l in f:
     if l[0] == 'o':
@@ -25,14 +25,25 @@ for l in f:
     if l[0:2] == 'vn':
         obj['normals'].append(parse_xyz(l[3:]))
         
-    if l[0:2] == 'f':
+    if l[0:1] == 'f':
+        face = {'type':0, 'vertices': [], 'normals':[]}
         arr = l[2:].strip().split(' ')
-        if len(arr) == 1:
-            obj["faces"]
+        face["type"] = len(arr)
+        
+        for i in arr:
+            n = i.split('//')
+            face["vertices"].append(int(n[0])-1)
+            
+            if len(n) == 2:
+                face["normals"].append(int(int(n[1])-1))
+                
+                
+        obj["faces"].append(face)
         
     if l[0] == 'n':
         output += ""     
         
-print json.dump(obj, open(sys.argv[2], 'w'))
+json.dump(obj, open(sys.argv[2], 'w'))
+print "Done"
     
 

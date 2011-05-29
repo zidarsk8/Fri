1b570ad36503579e5a5fe74cf3d3d2d2d38a7533
Smotko <ensmotko@gmail.com>
2011-05-30 00:33:05 +0200

    Got static dir working

--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    #(r'^admin/', include(admin.site.urls)),
+    (r'^$', 'web.views.index'),
     # Examples:
     # url(r'^$', 'Fri.views.home', name='home'),
     # url(r'^Fri/', include('Fri.foo.urls')),
@@ -15,3 +17,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += staticfiles_urlpatterns()

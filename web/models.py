a1722df6d779305ce4acf8fe6b2efe37242b5bbf
Smotko <ensmotko@gmail.com>
2011-05-30 12:30:16 +0200

    Added Tag model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length = 255)
+    description = models.CharField(max_length = 1000)
+    x = models.FloatField()
+    y = models.FloatField()
+    z = models.FloatField()
+    
+    def __unicode__(self):
+        return self.name + " " + self.description

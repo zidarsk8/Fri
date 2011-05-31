747121c2628df13a0e762749f7cc483b1cf701b0
Smotko <ensmotko@gmail.com>
2011-05-31 15:18:53 +0200

    The user can now add tags

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/v1/tags/add/', 'Fri.web.views.tag_add'),
+    url(r'^api/v1/tags/add', 'Fri.web.views.tag_add'),
     url(r'^api/v1/tags/(?P<tag>[-\w]+)$', 'Fri.web.views.tags_get'),
     url(r'^$', 'Fri.web.views.index'),
 )
